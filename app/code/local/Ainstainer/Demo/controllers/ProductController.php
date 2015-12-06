51f8a626ea0d3aa8908b9d89559e4ab9561bc85d
Kir <kirillvidyuk7@gmail.com>
2015-12-06 18:07:14 +0200

    controller overrides testing

---
+++ app/code/local/Ainstainer/Demo/controllers/ProductController.php
@@ -0,0 +1,9 @@
+<?php
+//require_once 'Mage/Catalog/controllers/ProductController.php';
+require_once(Mage::getModuleDir('controllers', 'Mage_Catalog').DS.'ProductController.php');
+class Ainstainer_Demo_ProductController extends Mage_Catalog_ProductController {
+
+//    public function viewAction {
+//        echo 'say hi';
+//    }
+}
