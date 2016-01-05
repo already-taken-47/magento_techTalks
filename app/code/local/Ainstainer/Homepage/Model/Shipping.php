e5181a1923cd1a9afb57e4544f7b315917ac787d
Kir <kirillvidyuk7@gmail.com>
2016-01-05 22:17:14 +0200

    simple five dollars shipment method

---
+++ app/code/local/Ainstainer/Homepage/Model/Shipping.php
@@ -0,0 +1,31 @@
+<?php
+
+class Ainstainer_Homepage_Model_Shipping extends Mage_Shipping_Model_Carrier_Abstract
+implements Mage_Shipping_Model_Carrier_Interface
+{
+    protected $_code = 'ain_five_shipping';
+
+    public function collectRates(Mage_Shipping_Model_Rate_Request $request)
+    {
+        $result = Mage::getModel('shipping/rate_result');
+        $result->append($this->_getDefaultRate());
+        return $result;
+    }
+
+    public function getAllowedMethods()
+    {
+        return array('ain_five_shipping' => $this->getConfigData('name'));
+    }
+
+    protected function _getDefaultRate()
+    {
+        $rate = Mage::getModel('shipping/rate_result_method');
+        $rate->setCarrier($this->_code);
+        $rate->setCarrierTitle($this->getConfigData('title'));
+        $rate->setMethod($this->_code);
+        $rate->setMethodTitle($this->getConfigData('name'));
+        $rate->setPrice($this->getConfigData('price'));
+        $rate->setCost(0);
+        return $rate;
+    }
+}
