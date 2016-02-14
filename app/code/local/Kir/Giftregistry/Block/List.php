ce4dedf253b0aefd969433458122021a1f5dd89e
Kir <kirillvidyuk7@gmail.com>
2016-02-14 17:27:09 +0200

    Installer script for giftregistry module

---
+++ app/code/local/Kir/Giftregistry/Block/List.php
@@ -0,0 +1,18 @@
+<?php
+class Kir_Giftregistry_Block_List extends Mage_Core_Block_Template
+{
+    public function getCustomerRegistries()
+    {
+        $collection = null;
+        $currentCustomer = Mage::getSingleton('customer/session')->getCustomer();
+        if($currentCustomer)
+        {
+            $collection = Mage::getModel('kir_giftregistry/entity')->getCollection()
+                ->addFieldToSelect('*')
+                ->addFieldToFilter('customer_id',
+                    $currentCustomer->getId());
+        }
+
+        return $collection;
+    }
+}
